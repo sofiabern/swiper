e524cfa71c15ab26a569592fb1f87a3034864153
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-04-11 00:37:45 +0300

    good3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
+// import Swiper JS
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper styles
+import 'swiper/css';
+
+
 const swiper = new Swiper(".swiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  modules: [Navigation, Pagination],
+  direction: 'horizontal',
+  spaceBetween: 32,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // on: {
+  //   reachEnd: function() {
+  //     document.querySelector(".swiper-button-next").disabled = true;
+  //   },
+  //   reachBeginning: function() {
+  //     document.querySelector(".swiper-button-prev").disabled = true;
+  //   },
+  // },
+});
