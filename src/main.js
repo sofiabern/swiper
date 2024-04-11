fbe056e6293c7a56d39a3e8384f3acca3503ddfe
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-04-11 13:07:55 +0300

    good4

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 // import Swiper JS
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 // import Swiper styles
 import 'swiper/css';
 
+// const swiperEl = document.querySelector('.swiper')
 
 const swiper = new Swiper(".swiper", {
-  modules: [Navigation, Pagination],
+  modules: [Navigation],
   direction: 'horizontal',
   spaceBetween: 32,
   navigation: {
@@ -14,12 +15,48 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 
-  // on: {
-  //   reachEnd: function() {
-  //     document.querySelector(".swiper-button-next").disabled = true;
-  //   },
-  //   reachBeginning: function() {
-  //     document.querySelector(".swiper-button-prev").disabled = true;
-  //   },
-  // },
+  mousewheel: {
+    enabled: true,
+    invert: true,
+  },
+  // не працює
+
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true
+},
+//   не працює
+
 });
+
+// // Змінна для збереження поточного стану видимості слайдера
+// let isSwiperInViewport = false;
+
+// // Створіть IntersectionObserver
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach(entry => {
+//     // Перевірте, чи слайдер видимий у вікні перегляду
+//     isSwiperInViewport = entry.isIntersecting;
+//   });
+// });
+
+// // Спостерігайте за елементом слайдера
+// observer.observe(swiperEl);
+
+
+// document.addEventListener('keydown', (event) => {
+//   // Перевірте, чи натиснута клавіша зі стрілкою вліво або вправо
+//   // і чи слайдер є у вікні перегляду
+//   if (isSwiperInViewport) {
+//     // console.log('куку')
+//     if (event.key === 'ArrowLeft') {
+//       // Перемістіть слайдер вліво
+//      swiper.slidePrev();
+//     } else if (event.key === 'ArrowRight') {
+//       // Перемістіть слайдер вправо
+//       swiper.slideNext();
+//     }
+//   }
+// });
